485d7944cb4e3e07db1a797f3e62be599fdacb05
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-10 19:22:45 -0500

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,9 @@
-using Documenter, DocumenterTools
+using Documenter, DocumenterTools, IncrementalAccumulators
 
 makedocs(
     modules = [IncrementalAccumulators],
-    format = if "pdf" in ARGS
-        IncrementalAccumulators.LaTeX(platform = "docker")
-    else
-        IncrementalAccumulators.HTML(
-            # Use clean URLs, unless built as a "local" build
-            prettyurls = !("local" in ARGS),
-            canonical = "https://juliadocs.github.io/IncrementalAccumulators.jl/stable/",
-            assets = ["assets/favicon.ico"],
-            analytics = "UA-136089579-2",
-            highlights = ["yaml"],
-            ansicolor = true,
-        )
-    end,
-    build = ("pdf" in ARGS) ? "build-pdf" : "build",
-    debug = ("pdf" in ARGS),
-    clean = false,
     sitename = "IncrementalAccumulators.jl",
-    authors = "Jeffrey Sarnoff and contributors",
+    authors = ["Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>", "and contributors"],
     pages = [
         "Home" => "index.md",
         "Manual" => Any[
@@ -29,32 +13,11 @@ makedocs(
         "contributing.md",
         "citing.md",
     ],
-    strict = !("strict=false" in ARGS),
-    doctest = ("doctest=only" in ARGS) ? :only : true,
 )
 
-if "pdf" in ARGS
-    # hack to only deploy the actual pdf-file
-    mkpath(joinpath(@__DIR__, "build-pdf", "commit"))
-    let files = readdir(joinpath(@__DIR__, "build-pdf"))
-        for f in files
-            if startswith(f, "IncrementalAccumulators.jl") && endswith(f, ".pdf")
-                mv(joinpath(@__DIR__, "build-pdf", f),
-                joinpath(@__DIR__, "build-pdf", "commit", f))
-            end
-        end
-    end
-    deploydocs(
-        repo = "github.com/JeffreySarnoff/IncrementalAccumulators.jl.git",
-        target = "pdf/build-pdf/commit",
-        branch = "gh-pages-pdf",
-        forcepush = true,
-    )
-else
-    deploydocs(
-        repo = "github.com/JeffreySarnoff/IncrementalAccumulators.jl.git",
-        target = "build",
-        push_preview = true,
-    )
-end
+deploydocs(
+    repo = "github.com/JeffreySarnoff/IncrementalAccumulators.jl.git",
+    target = "build"
+    push_preview = true,
+)
 
