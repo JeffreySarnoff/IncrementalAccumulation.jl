492ab19359820dd4c0585b2f8f04e45127d9040a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-10 19:37:24 -0500

    Create types.jl

---
+++ src/types.jl
@@ -0,0 +1,22 @@
+#= ---- abstract types ---- =#
+
+"""
+    Accumulator{T,F}
+"""
+Accumulator
+
+abstract type Accumulator{T,F} <: Function end
+
+#= ---- generalized sequence ---- =#
+
+"""
+    Seq
+"""
+const Seq = Union{AbstractArray{T,N},NTuple{N,T}} where {N,T}
+
+"""
+    seq
+"""
+seq(x::AbstractArray{T,N}) where {N,T} = x
+seq(x::NTuple{N,T}) where {N,T} = x
+
