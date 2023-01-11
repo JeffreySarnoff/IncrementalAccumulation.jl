10927c4766fca63ee9c4ad4bcc7b57496f57b312
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-10 19:39:11 -0500

    Update IncrementalAccumulators.jl

--- src/IncrementalAccumulators.jl
+++ src/IncrementalAccumulators.jl
@@ -2,27 +2,11 @@ module IncrementalAccumulators
 
 using StatsBase, VectorizedStatistics
 
-"""
-    Accumulator{T,F}
-""" 
-abstract type Accumulator{T,F} <: Function end
-
-#= ---- generalized sequence ---- =#
-
-"""
-    Seq
-"""
-const Seq = Union{AbstractArray{T,N},NTuple{N,T}} where {N,T}
-
-"""
-    seq
-"""
-seq(x::AbstractArray{T,N}) where {N,T} = x
-seq(x::NTuple{N,T}) where {N,T} = x
-
-#= ---- Julia code ---- =#
+#= ---- functions and functors ---- =#
 
 include("support.jl")
+include("types.jl")
+
 include("accumulators.jl")
 
 end #  IncrementalAccumulators
