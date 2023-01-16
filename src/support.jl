3c736498485d1df0bd32679119ad6b1866855fe5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-16 15:44:15 -0500

    Update support.jl

--- src/support.jl
+++ src/support.jl
@@ -1,2 +1,9 @@
 const AccNum = Float64
 
+# from within a Union
+union_types(x::Union) = (x.a, union_types(x.b)...)
+union_types(x::Type) = (x,)
+union_common(x::Union) = setdiff(union_types(T),(Missing,Nothing))
+commontype(x::Union) = union_common(x)[1]
+commontype(::Type{T}) = T
+
