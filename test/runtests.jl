cf0f80fae17db86ec41ca894b5add50f7b745027
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-16 23:13:28 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,32 @@
 using IncrementalAccumulators, Test
 
-@test 1 == 1
+ints = collect(1:7)
+floats = collect(1.0:7.0)
 
+intsmixed = ints
+intsmixed[6] = -intsmixed[6]
+
+floatsmixed = floats
+floatsmixed[6] = -floatsmixed[6]
+
+intsmissing = Union{Missing,Int}[ints...]
+intsmissing[3] = missing
+intsmissing[4] = missing
+
+floatsmissing = Union{Missing,Float64}[ints...]
+floatsmissing[3] = missing
+floatsmissing[4] = missing
+
+cumsum_ints = cumsum(ints)
+cumsum_floats = cumsum(floats)
+
+cumsum_intsmixed = cumsum(intsmixed)
+cumsum_floatsmixed = cumsum(floatsmixed)
+
+accsum1 = AccSum(Int)
+accsum2 = AccSum(Float64)
+
+fnsum(acc::Accumulator, x) = ismissing(x) ? acc.sum : x
+
+accsum = AccSum(Union{Missing, Float64}; fn=fnsum)
 
